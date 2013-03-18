b080067e609ba752294b1c3023b85614b09b73bc
Greg Ledbetter <gsledbetter@gmail.com>
2013-03-18 09:02:43 -0400

    Initial Commit

---
+++ pedigree/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pedigree
+//
+//  Created by jtq6 on 3/18/13.
+//  Copyright (c) 2013 jtq6. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
